8a60d0fd81ead6892071fe5f6e36fb3951760be1
Mike Jobson <mike.jobson@mac.com>
2025-09-07 13:14:10 +0100

    crestron-discover 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/crestron-discover.rb
+++ Formula/crestron-discover.rb
@@ -1,9 +1,9 @@
 class CrestronDiscover < Formula
   desc "Short description of your tool"
   homepage "https://github.com/mikejobson/crestron-discover"
-  url "https://github.com/mikejobson/crestron-discover/releases/download/v0.1.0/crestron-discover"
-  sha256 "2c5b39f11f688988eea4d2e3fce89235a2728f8df6998a1b220ec697ca08de42"
-  version "0.1.0"
+  url "https://github.com/mikejobson/crestron-discover/releases/download/v0.1.1/crestron-discover"
+  sha256 "cef39ffcfca6016d80d3acfe46d14efb41ff87baf06d39093cd09894cb443a25"
+  version "0.1.1"
   license "MIT" # or your license
 
   def install
