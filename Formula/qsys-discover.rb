6968f73bf700655eb773c750f0ec83b74ad5e32d
Mike Jobson <mike.jobson@mac.com>
2026-03-23 11:00:03 +0000

    qsys-discover 0.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/qsys-discover.rb
+++ Formula/qsys-discover.rb
@@ -1,9 +1,9 @@
 class QsysDiscover < Formula
   desc "TBD"
   homepage "https://github.com/mikejobson/qsys-discover"
-  url "https://github.com/mikejobson/qsys-discover/releases/download/initial/qsys-discover"
-  version "0.0.0"
-  sha256 "dummysha256"
+  url "https://github.com/mikejobson/qsys-discover/releases/download/v0.2.0/qsys-discover"
+  version "0.2.0"
+  sha256 "5afc7a13da3338f07c813231c9fc946ddc3667f5c858b77ff1d51a3b1b742608"
 
   def install
     bin.install "qsys-discover"
