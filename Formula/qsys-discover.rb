9b68c4d370d9e5c6ca5d6d539e0224211c91f33a
Mike Jobson <mike.jobson@mac.com>
2026-03-23 10:58:51 +0000

    Add QsysDiscover formula for Homebrew

---
+++ Formula/qsys-discover.rb
@@ -0,0 +1,12 @@
+class QsysDiscover < Formula
+  desc "TBD"
+  homepage "https://github.com/mikejobson/qsys-discover"
+  url "https://github.com/mikejobson/qsys-discover/releases/download/initial/qsys-discover"
+  version "0.0.0"
+  sha256 "dummysha256"
+
+  def install
+    bin.install "qsys-discover"
+  end
+end
+
