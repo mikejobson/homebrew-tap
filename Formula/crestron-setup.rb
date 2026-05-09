7283e466fc8df376973129b5a9538564edacab43
Mike Jobson <mike.jobson@mac.com>
2026-05-09 11:43:28 +0100

    Add CrestronSetup formula for CLI tool

---
+++ Formula/crestron-setup.rb
@@ -0,0 +1,11 @@
+class CrestronSetup < Formula
+  desc "CLI tool for automated Crestron processor provisioning"
+  homepage "https://github.com/mikejobson/Crestron-Processor-Setup"
+  url "https://github.com/mikejobson/Crestron-Processor-Setup/releases/download/v0.1.0/crestron-setup-macos"
+  version "0.1.0"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
+  def install
+    bin.install "crestron-setup-macos" => "crestron-setup"
+  end
+end
