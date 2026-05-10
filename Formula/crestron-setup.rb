20d8b3cac5fc41e0c07755c161b6792a6c8d761d
Mike Jobson <mike.jobson@mac.com>
2026-05-10 09:32:52 +0200

    crestron-setup 1.0.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/crestron-setup.rb
+++ Formula/crestron-setup.rb
@@ -1,9 +1,9 @@
 class CrestronSetup < Formula
   desc "CLI tool for automated Crestron processor provisioning"
   homepage "https://github.com/mikejobson/Crestron-Processor-Setup"
-  url "https://github.com/mikejobson/Crestron-Processor-Setup/releases/download/v1.0.4/crestron-setup-macos"
-  version "1.0.4"
-  sha256 "6023d14e5cb455f84aae3b83b903ce0d8d5b4e7bf3914162afbb2bb635976cda"
+  url "https://github.com/mikejobson/Crestron-Processor-Setup/releases/download/v1.0.5/crestron-setup-macos"
+  version "1.0.5"
+  sha256 "79ab8dba5fb3dc91b43312a9f6670d12a7bc566e5d167b54ecbfda0d0612b7f5"
 
   def install
     bin.install "crestron-setup-macos" => "crestron-setup"
